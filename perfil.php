4cd6a39825e6bb4bcd9a41e433162c19a3486356
Your Name <jolekovox@gmail.com>
2022-03-23 17:06:35 +0100

    fuentes

--- perfil.php
+++ perfil.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="resources/css/style.css">
     <link rel="shortcut icon" href="./resources/img/favicon.ico">
-    <title>CINITO</title>
+    <title>Cinito</title>
 </head>
 <body>
     <div id="container">
