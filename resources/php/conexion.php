b5e1e6f51ea3e332b2eaae7717981fa93b2bcb85
Your Name <jolekovox@gmail.com>
2022-03-23 17:20:13 +0100

    nada

--- resources/php/conexion.php
+++ resources/php/conexion.php
@@ -5,14 +5,4 @@
     $nombreBD="bd1";
 
     $conexion = mysqli_connect($servidor,$usuario,$pass) or die("Error de conexión");
-
-    /*  Crear las tablas para la comprobación
-        CREATE TABLE usuarios ( id int(20) AUTO_INCREMENT,
-                                username varchar(10) NOT NULL,
-                                pass varchar(60) NOT NULL,
-                                email varchar(255) NOT NULL,
-                                fecha varchar(255)NOT NULL, 
-                                PRIMARY KEY (id),
-                                UNIQUE (username,email));
-    */  
 ?>
